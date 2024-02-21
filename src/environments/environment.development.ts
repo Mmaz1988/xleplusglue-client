2cd6e5aa03e1527b5a7343c3ecb46ce193d3da48
mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2024-02-21 11:10:58 +0100

    fixed url error

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+//For ng serve
+export const environment = {
+  stanzapage : 'http://localhost:8002',
+  ud2sempage : 'http://localhost:8004',
+  multisempage : 'http://localhost:8006'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+//For ng build (for deployment)
+export const environment = {
+  stanzapage : 'http://xle.uni-konstanz.de:8002',
+  ud2sempage : 'http://xle.uni-konstanz.de:8003',
+  multisempage : 'http://xle.uni-konstanz.de:8004'
+};
